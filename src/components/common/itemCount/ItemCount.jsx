5a8826a96a9995c428bd4f244380b23ae4d94f4c
Cristianscamilo <cristianscamilo9@gmail.com>
2024-02-28 16:20:03 -0500

    Segunda entrega

--- src/components/common/itemCount/ItemCount.jsx
+++ src/components/common/itemCount/ItemCount.jsx
@@ -1,12 +1,13 @@
 const ItemCount = ({ counter, addOne, subOne, stock }) => {
   return (
     <>
+    <h3>
+        {counter} de {stock} Unidades en Stock
+      </h3>
       <button onClick={addOne} disabled={counter < stock ? false : true}>
         Añadir unidad
       </button>
-      <h4>
-        {counter} de {stock} Unidades en Stock
-      </h4>
+      
       <button onClick={subOne} disabled={counter === 1 ? true : false}>
         Restar unidad
       </button>
--- src/components/pages/itemDetalContainer/ItemDetail.jsx
+++ src/components/pages/itemDetalContainer/ItemDetail.jsx
@@ -1,17 +1,21 @@
+import styles from "./ItemDetail.module.css";
 import ItemCountContainer from "../../common/itemCount/ItemCountContainer";
+
 
 const ItemDetail = ({ item }) => {
   const { title, img, description, price, stock } = item;
 
   return (
     <>
-      <article>
-        <h3>{title}</h3>
-        <img src={img} alt="" />
+      <article className={styles.cartDetail}>
+        <h3 className={styles.titleCartDeail}>{title}</h3>
+        <img src={img} alt="" className={styles.imgCartDetail} />
         <p>{description}</p>
         <p>Precio: ${price}</p>
+        <div className={styles.containerCount}>
         <ItemCountContainer stock={stock} />
         <button> Comprar </button>
+        </div>
       </article>
     </>
   );
