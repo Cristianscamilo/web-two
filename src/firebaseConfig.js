7c24eabef78fc6c4c3f7174daf08a4a02505eb9a
Cristianscamilo <cristianscamilo9@gmail.com>
2024-03-23 18:54:23 -0500

    Prueba firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,15 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore"
+// const firebaseConfig = {
+//   apiKey: "AIzaSyD9Tre-BCUdpdYyIC5rByXSRvs4yYLjems",
+//   authDomain: "comercecp-zero.firebaseapp.com",
+//   projectId: "comercecp-zero",
+//   storageBucket: "comercecp-zero.appspot.com",
+//   messagingSenderId: "297630139804",
+//   appId: "1:297630139804:web:e344a61915863a4b9b3459"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// export const db = getFirestore( app )
