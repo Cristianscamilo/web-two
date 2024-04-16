d59c58e09e621cf9cbaad2c267a430204fa471ad
Cristianscamilo <cristianscamilo9@gmail.com>
2024-04-16 17:16:43 -0500

    Se ajusta MUI en checkout

--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,3 +1,7 @@
+import Stack from '@mui/joy/Stack';
+import Input from '@mui/joy/Input';
+import Button from '@mui/joy/Button';
+
 export const Checkout = ({ envioDeFormulario, capturar, orderId }) => {
   return (
     <div>
@@ -5,27 +9,30 @@ export const Checkout = ({ envioDeFormulario, capturar, orderId }) => {
         <h2>Gracias por tu compra, el numero de confirmación es {orderId}</h2>
       ) : (
         <form onSubmit={envioDeFormulario}>
-          <input
+          <h3 style={{marginLeft:"50px", marginTop:"20px"}}>FINALIZA TU COMPRA</h3>
+          <Stack spacing={2} sx={{ width: 800, margin: 6, mb: 20}}>
+          <Input
             type="text"
             placeholder="Ingresa tu nombre"
             onChange={capturar}
             name="name"
           />
 
-          <input
+          <Input
             type="text"
             placeholder="Ingresa tu telefono"
             onChange={capturar}
             name="phone"
           />
-          <input
+          <Input
             type="text"
             placeholder="Ingresa tu email"
             onChange={capturar}
             name="email"
           />
 
-          <button type="submit">Comprar</button>
+          <Button type="submit">Comprar</Button>
+          </Stack>
         </form>
       )}
     </div>
